2a49196b80b4ac802508b476609fa3bdd7538236
xu756 <756334744@qq.com>
2022-12-21 00:13:30 +0800

    重新生成

--- api/user/internal/svc/servicecontext.go
+++ api/user/internal/svc/servicecontext.go
@@ -1,15 +1,19 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 	"mylooklook/api/user/internal/config"
+	"mylooklook/api/user/internal/model"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUserModel(sqlx.NewMysql(c.Mysql.DataSource), c.Cache),
 	}
 }
