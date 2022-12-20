3f09fd87d4dbf3df6e7203d1ee12d6cc7252d358
xu756 <756334744@qq.com>
2022-12-21 00:13:09 +0800

    配置

--- api/user/internal/config/config.go
+++ api/user/internal/config/config.go
@@ -1,7 +1,15 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/rest"
+)
 
 type Config struct {
 	rest.RestConf
+	Mysql struct { // 数据库配置，除mysql外，可能还有mongo等其他数据库
+		DataSource string // mysql链接地址，满足 $user:$password@tcp($ip:$port)/$db?$queries 格式即可
+	}
+	Cache cache.CacheConf // redis缓存
+
 }
