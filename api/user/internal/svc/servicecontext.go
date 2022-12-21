4987bb909b43f16718573d8671d61359f545eba8
xu756 <756334744@qq.com>
2022-12-21 22:44:00 +0800

    修改

--- api/user/internal/svc/servicecontext.go
+++ api/user/internal/svc/servicecontext.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/core/syncx"
 	"mylooklook/api/user/internal/config"
 	"mylooklook/api/user/internal/model"
 )
@@ -11,9 +12,14 @@ type ServiceContext struct {
 	UserModel model.UserModel
 }
 
+var connManager = syncx.NewResourceManager()
+
 func NewServiceContext(c config.Config) *ServiceContext {
+
 	return &ServiceContext{
 		Config:    c,
 		UserModel: model.NewUserModel(sqlx.NewMysql(c.Mysql.DataSource), c.Cache),
 	}
 }
+
+// 配置连接池
