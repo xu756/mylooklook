f33a8792dd25b2846d0fbf4c0ec41bd92f2359de
xu756 <756334744@qq.com>
2022-12-21 19:19:24 +0800

    Changes

---
+++ api/user/internal/model/usermodel.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ UserModel = (*customUserModel)(nil)
+
+type (
+	// UserModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customUserModel.
+	UserModel interface {
+		userModel
+	}
+
+	customUserModel struct {
+		*defaultUserModel
+	}
+)
+
+// NewUserModel returns a model for the database table.
+func NewUserModel(conn sqlx.SqlConn, c cache.CacheConf) UserModel {
+	// 配置连接池
+
+	return &customUserModel{
+		defaultUserModel: newUserModel(conn, c),
+	}
+}
