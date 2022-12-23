89ccda6e6ad4f064af635f51a684ef91125e3468
xu756 <756334744@qq.com>
2022-12-24 00:56:22 +0800

    jwt2

--- api/user/internal/logic/userloginlogic.go
+++ api/user/internal/logic/userloginlogic.go
@@ -3,9 +3,11 @@ package logic
 import (
 	"context"
 	"errors"
+	"github.com/golang-jwt/jwt/v4"
 	"mylooklook/api/user/internal/svc"
 	"mylooklook/api/user/internal/types"
 	"strings"
+	"time"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -23,8 +25,23 @@ func NewUserLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLog
 		svcCtx: svcCtx,
 	}
 }
+func (l *UserLoginLogic) getJwtToken(secretKey string, iat, seconds, userId int64) (string, error) {
+	claims := make(jwt.MapClaims)
+	claims["exp"] = iat + seconds
+	claims["iat"] = iat
+	claims["userId"] = userId
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims = claims
+	return token.SignedString([]byte(secretKey))
+}
 
 func (l *UserLoginLogic) UserLogin(req *types.UserLogin) (resp *types.Res, err error) {
+	now := time.Now().Unix()
+	accessExpire := l.svcCtx.Config.Auth.AccessExpire
+	jwtToken, er := l.getJwtToken(l.svcCtx.Config.Auth.AccessSecret, now, l.svcCtx.Config.Auth.AccessExpire, userInfo.Id)
+	if e != nil {
+		return nil, er
+	}
 	if len(strings.TrimSpace(req.Username)) == 0 || len(strings.TrimSpace(req.Password)) == 0 {
 		return nil, errors.New("参数错误")
 	}
