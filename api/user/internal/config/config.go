b45e5585e112e2af5054a922b5bc3f511c7fb39f
xu756 <756334744@qq.com>
2022-12-23 23:52:33 +0800

    配置东西

--- api/user/internal/config/config.go
+++ api/user/internal/config/config.go
@@ -7,6 +7,10 @@ import (
 
 type Config struct {
 	rest.RestConf
+	Auth struct { // jwt鉴权配置
+		AccessSecret string // jwt密钥
+		AccessExpire int64  // 有效期，单位：秒
+	}
 	Mysql struct { // 数据库配置，除mysql外，可能还有mongo等其他数据库
 		DataSource string // mysql链接地址，满足 $user:$password@tcp($ip:$port)/$db?$queries 格式即可
 	}
